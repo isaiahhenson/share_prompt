012eaf462a2c1a4fadca6c657869e924b589021f
Isaiah Henson <Devpuddles@gmail.com>
2023-05-17 11:33:16 -0400

    created api, auth and nextauth folders also made modifications to the home page.

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,7 @@
-import '@styles/globals.css'
+import '@styles/globals.css';
+
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
 
 export const metadata = {
     title: "Promptopia",
@@ -10,13 +13,16 @@ const RootLayout = ({children}) => {
   return (
     <html lang="en">
         <body>
+            <Provider>
             <div className="main">
                 <div className="gradient"></div>
             </div>
 
             <main className="app">
+                <Nav />
                 {children}
             </main>
+            </Provider>
         </body>
     </html>
   )
